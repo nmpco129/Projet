99837d60eef864e5fc6bc4900c7631f10438a8c0
DESKTOP-N7VNA88\admin <peitfingers@gmail.com>
2017-01-16 15:52:17 +0700

    fix upload hinh(1)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,3 +6,25 @@ CarrierWave.configure do |config|
   config.ftp_url = ENV['ftp_url']
   config.ftp_passive = true
 end
+
+class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  if Rails.env.production?
+    storage :ftp
+  else
+    storage :file
+  end
+
+  version :medium do
+    process resize_to_fill: [480, 360]
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+end
